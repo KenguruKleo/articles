615117c87691e407be059a4d910e9cf847c8d442
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2018-05-16 22:23:54 +0300

    add config dev options

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+let devConfig = {};
+
+if (fs.existsSync(path.resolve(__dirname, './config.dev.js'))) {
+  devConfig = require('./config.dev'); // eslint-disable-line
+}
+
 module.exports = {
-  APP_PORT: process.env.PORT || 5000,
-  logLevel: process.env.APP_LOG_LEVEL || 'info',
-  jwt_secret: process.env.APP_JWT_SECRET || 'you must specify JWT secret',
-  NEWS_API_KEY: process.env.NEWS_API_KEY || 'you must specify newsapi.org KEY',
-  newsResourceURL: 'https://newsapi.org/v2',
+  APP_PORT: devConfig.APP_PORT || process.env.PORT || 5000,
+  logLevel: devConfig.logLevel || process.env.APP_LOG_LEVEL || 'info',
+  jwt_secret: devConfig.jwt_secret || process.env.APP_JWT_SECRET || 'you must specify JWT secret',
+  NEWS_API_KEY: devConfig.NEWS_API_KEY || process.env.NEWS_API_KEY || 'you must specify newsapi.org KEY',
+  newsResourceURL: devConfig.newsResourceURL || 'https://newsapi.org/v2',
 };
